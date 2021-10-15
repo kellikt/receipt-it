f2814ba66b972e3511ada01a59834a5160c11a24
Rexter Delos Santos <rexterds@hawaii.edu>
2021-10-14 14:09:50 -1000

    changed tab names

--- app/src/main/java/com/example/receipt_it/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/receipt_it/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,8 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.example.receipt_it.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_1,
+    R.string.tab_2
 )
 
 /**
